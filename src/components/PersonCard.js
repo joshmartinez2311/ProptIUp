959ccb710cf9f3842a36311c8f3af11b4da551c4
Joshthe1st <88605608+Joshthe1st@users.noreply.github.com>
2023-05-14 17:07:18 -0700

    second commit

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const PersonCard = (props) => {
+    //deconstructing array and setting it equals to props
     const {firstName, lastName, age, hairColor} = props
+    //  setters and getters to useState and passing age as an argument
+    const [currentAge, setCurrentAge] = useState(age)
     return (
         <div>
             <h1>{lastName} , {firstName}</h1>
-            <p> age: {age}</p>
+            <p> age: {currentAge}</p>
             <p>Hair Color: {hairColor}</p>
+        {/* adding an onclick event that takes currentAge and adds 1 on click */}
+            <button onClick={ (event) => setCurrentAge(currentAge + 1 ) }>BirthDay Button for {firstName} {lastName}</button>
         </div>
     )
 }
